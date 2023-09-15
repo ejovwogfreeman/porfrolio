ed807093fd07aa85c7e68bf223145ff9d5d3fc33
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2023-09-15 19:41:47 +0100

    messsage

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -13,14 +13,12 @@ const SocialMedia = ({ color }) => {
     <div className="social-icons" style={{ color: color, borderColor: color }}>
       <a
         href="https://www.youtube.com/@thegbmedia/"
-        target="_blank"
         style={{ color: color, borderColor: color }}
       >
         <AiFillYoutube />
       </a>
       <a
         href="https://www.facebook.com/ejovwo.gfreeman/"
-        target="_blank"
         style={{ color: color, borderColor: color }}
       >
         <AiFillFacebook />
@@ -33,21 +31,18 @@ const SocialMedia = ({ color }) => {
       </a>
       <a
         href="https://www.twitter.com/ejovwogfreeman/"
-        target="_blank"
         style={{ color: color, borderColor: color }}
       >
         <FaTwitterSquare />
       </a>
       <a
         href="https://www.linkedin.com/in/godbless-ejovwo/"
-        target="_blank"
         style={{ color: color, borderColor: color }}
       >
         <AiFillLinkedin />
       </a>
       <a
         href="https://www.github.com/ejovwogfreeman/"
-        target="_blank"
         style={{ color: color, borderColor: color }}
       >
         <FaGithubSquare />
